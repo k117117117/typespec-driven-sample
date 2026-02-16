0f5ba1de6aaf9603e542531719de085d66a87d1b
k117117117 <130058514+k117117117@users.noreply.github.com>
2026-02-16 21:48:13 +0900

    HelloWorld内のリンクテキストを変更

--- admin.frontend/src/helloWorld/HelloWorld.tsx
+++ admin.frontend/src/helloWorld/HelloWorld.tsx
@@ -4,8 +4,8 @@ export const HelloWorld = () => (
     <div>
         <h1>Hello, World!</h1>
         <p>CustomRoutesコンポーネントを使うと、独自のルートも追加することができます。</p>
-        <Link href="https://marmelab.com/react-admin/CustomRoutes.html" target="_blank" rel="noopener">詳細はこちら</Link>
+        <Link href="https://marmelab.com/react-admin/CustomRoutes.html" target="_blank" rel="noopener">CustomRoutes component</Link>
         <p>カスタムルートは自動的にはMenuに追加されないので以下の方法で追加する必要がある</p>
-        <Link href="https://marmelab.com/react-admin/CustomRoutes.html#adding-custom-routes-to-the-menu" target="_blank" rel="noopener">詳細はこちら</Link>
+        <Link href="https://marmelab.com/react-admin/CustomRoutes.html#adding-custom-routes-to-the-menu" target="_blank" rel="noopener">Adding Custom Routes to the Menu</Link>
     </div>
 );
